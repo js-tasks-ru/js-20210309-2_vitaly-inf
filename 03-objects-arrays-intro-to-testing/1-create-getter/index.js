354e7b7900248d30fd43c3e2f031861bf3cbbb45
Vitaly <vitaly.inf@yandex.ru>
2021-03-22 18:40:12 +0300

    Модуль2

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,16 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  return function setGetter(obj) {
+
+    const field = path.split('.');
+    for (let i = 0; i < field.length; i++) {
+      obj = obj[field[i]];
+      if(!obj){
+          return undefined;
+      }
+    }   
+    return obj;
+  };
 
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,24 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === undefined){
+    return string;
+  }
+  if (size === 0){
+    return '';
+  }
+  let count = 1;
+  return string.split('').reduce((prev,a,i,arr) => {
+    if (arr[i-1] === a){
+      if(count < size){
+        count++;
+        prev.push(a);
+      }} else {
+      count = 1;
+      prev.push(a);
+    }
+  
 
+    return prev;
+  }, []).join('');  
 }
